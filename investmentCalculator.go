5fa1401a4b2f8aab74b52910b18e09a1341c2f45
mohitgaur310 <mohitgaur310@gmail.com>
2024-10-23 23:46:22 +0530

    wip go

--- investmentCalculator.go
+++ investmentCalculator.go
@@ -5,9 +5,11 @@ import (
 	"math"
 )
 func main() {
-	var investMentAmt=1000;
-	var expectedReturnRate=5.5;
-	var years =10
-	var futureValue =float64(investMentAmt)* math.Pow(1+ expectedReturnRate/100,float64(years))
-	fmt.Println(futureValue)
+	var investMentAmt float64 =1000;
+	const inflationRate = 2.5
+	expectedReturnRate :=5.5;
+	var years float64 =10
+	var futureValue =investMentAmt* math.Pow(1+ expectedReturnRate/100,years)
+	futureRealValue := futureValue/math.Pow(1+inflationRate/100,years)
+	fmt.Println(futureValue,futureRealValue)
 }
