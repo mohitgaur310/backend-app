bde7594612c3fa674a7b3576d5dad523f7685269
mohitgaur310 <mohitgaur310@gmail.com>
2024-10-26 22:53:41 +0530

    create a func for cal fuure value

--- profitCalculator.go
+++ profitCalculator.go
@@ -16,16 +16,18 @@ func main() {
 	outputText("Tex rate :")
 	fmt.Scan(&texRate);
 
-	ebt:= revenue-expenses;
-	profile := ebt*(1-texRate/100)
+	ebt,profile:= calculateFutureValue(revenue,expenses,texRate)
 	ratioString := fmt.Sprintf("the ratio of the ebt and profit is  %.2f",ebt /profile) 
-
-
-
 
 	fmt.Println(ebt,profile,ratioString)
 }
 
 func outputText(text string){
 	fmt.Println(text)
+}
+
+func calculateFutureValue(revenue float64,expenses float64,texRate float64) (float64,float64) {
+	ebt:= revenue-expenses;
+	profile := ebt*(1-texRate/100)
+	return ebt,profile
 }
