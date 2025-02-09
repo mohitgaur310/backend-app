ef0dda578a29463fff4bd057c0f637b4f1e1e39e
mohitgaur310 <mohitgaur310@gmail.com>
2025-02-09 23:53:47 +0530

    user input and limitation

--- investmentCalculator.go
+++ investmentCalculator.go
@@ -5,10 +5,17 @@ import (
 	"math"
 )
 func main() {
-	var investMentAmt float64 =1000;
+	var investMentAmt float64 
 	const inflationRate = 2.5
 	expectedReturnRate :=5.5;
 	var years float64 =10
+	fmt.Print(("please enter investment value :"))
+	fmt.Scan(&investMentAmt)
+
+	fmt.Print(("please enter Year : "))
+	fmt.Scan(&years)
+	fmt.Print(("please enter Expected Return Rate : "))
+	fmt.Scan(&expectedReturnRate)
 	var futureValue =investMentAmt* math.Pow(1+ expectedReturnRate/100,years)
 	futureRealValue := futureValue/math.Pow(1+inflationRate/100,years)
 	fmt.Println(futureValue,futureRealValue)
